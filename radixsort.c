1b654ed16770beec583d712a4d681febb29223d4
Jiya <222jiyatiwari@gmail.com>
2025-08-03 22:11:22 +0530

    radixsort.c

--- radixsort.c
+++ radixsort.c
@@ -8,26 +8,22 @@ int getMax(int arr[], int n) {
     return mx;
 }
 
+
 void countSort(int arr[], int n, int exp) {
     int output[n];
     int i, count[10] = {0};
 
-    // Count occurrences of digits at current place
     for (i = 0; i < n; i++)
         count[(arr[i] / exp) % 10]++;
 
-    // Transform count[i] so that count[i] now contains actual
-    // position of this digit in output[]
     for (i = 1; i < 10; i++)
         count[i] += count[i - 1];
 
-    // Build the output array
     for (i = n - 1; i >= 0; i--) {
         output[count[(arr[i] / exp) % 10] - 1] = arr[i];
         count[(arr[i] / exp) % 10]--;
     }
 
-    // Copy the output array to arr[], so arr[] now contains sorted numbers
     for (i = 0; i < n; i++)
         arr[i] = output[i];
 }
@@ -35,16 +31,17 @@ void countSort(int arr[], int n, int exp) {
 void radixSort(int arr[], int n) {
     int m = getMax(arr, n);
 
-    // Do counting sort for every digit
     for (int exp = 1; m / exp > 0; exp *= 10)
         countSort(arr, n, exp);
 }
+
 
 void print(int arr[], int n) {
     for (int i = 0; i < n; i++)
         printf("%d ", arr[i]);
     printf("\n");
 }
+
 
 int main() {
     int arr[] = {170, 45, 75, 90, 802, 24, 2, 66};
