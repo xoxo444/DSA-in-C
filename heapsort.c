5563230d5c10e03e0112c72a21fcba6d418439a7
Jiya <222jiyatiwari@gmail.com>
2025-08-05 20:30:46 +0530

    heapsort.c

--- heapsort.c
+++ heapsort.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 void heapify(int arr[], int n, int i) {
-    int largest = i;          // Initialize largest as root
-    int left = 2 * i + 1;     // left child
-    int right = 2 * i + 2;    // right child
+    int largest = i;          
+    int left = 2 * i + 1;     
+    int right = 2 * i + 2;   
 
     if (left < n && arr[left] > arr[largest])
         largest = left;
