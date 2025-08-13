4691f505755205f07db9802e2154b17bd047a949
Jiya <222jiyatiwari@gmail.com>
2025-08-13 19:51:29 +0530

    countingsort.c

--- countingsort.c
+++ countingsort.c
@@ -2,44 +2,37 @@
 #include <stdlib.h>
 
 void countingSort(int arr[], int n) {
-    // Find the maximum element
     int max = arr[0];
     for (int i = 1; i < n; i++) {
         if (arr[i] > max)
             max = arr[i];
     }
 
-    // Create count array
     int *count = (int*)calloc(max + 1, sizeof(int));
 
-    // Store count of each element
     for (int i = 0; i < n; i++) {
         count[arr[i]]++;
     }
 
-    // Modify count array to store cumulative counts
     for (int i = 1; i <= max; i++) {
         count[i] += count[i - 1];
     }
 
-    // Output array
     int *output = (int*)malloc(n * sizeof(int));
 
-    // Build the output array (stable sorting)
     for (int i = n - 1; i >= 0; i--) {
         output[count[arr[i]] - 1] = arr[i];
         count[arr[i]]--;
     }
 
-    // Copy sorted array back to original
     for (int i = 0; i < n; i++) {
         arr[i] = output[i];
     }
 
-    // Free memory
     free(count);
     free(output);
 }
+
 
 int main() {
     int arr[] = {4, 2, 2, 8, 3, 3, 1};
